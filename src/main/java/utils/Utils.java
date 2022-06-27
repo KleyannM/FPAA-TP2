d04621359b1f79f85c370a141bdbfb2a1d9ca5af
KleyannM <kleyann1@gmail.com>
2022-06-27 02:23:25 -0300

    Atualização relatório

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -29,7 +29,7 @@ public class Utils {
 
     public static List<Rolo> getRolos(String caminho) throws NullPointerException {
         File file = new File(caminho);
-        Scanner scanner = null;
+        Scanner scanner;
         try {
             scanner = new Scanner(file);
         } catch (FileNotFoundException e) {
